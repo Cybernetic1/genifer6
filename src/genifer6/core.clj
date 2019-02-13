db80281f4d5f5458c60771bd2fc1b9839c871dee
Cybernetic1 <generic.intelligence@gmail.com>
2019-02-13 20:55:51 +0800

    rules

--- src/genifer6/core.clj
+++ src/genifer6/core.clj
@@ -7,14 +7,16 @@
   [& args]
   (println "Welcome to Genifer 6"))
 
-(defrecord Happy [name level])
+;; In the long term, the rules engine needs to be rewritten.
+
+(defrecord Happy [name])
 
 (defrecord Loves [name1 name2])
 
 (defrule rule1
-  [Happy (= :high level)]
+  [Happy (= ?name name)]
   =>
-  (println "High level happy detected!"))
+  (println ?name "is happy!"))
 
 (defrule rule2
   [Happy (= ?name1 name)]
@@ -27,20 +29,25 @@
   [Loves (= ?name1 name1) (= ?name2 name2)]
   [Loves (= ?name2 name1) (= ?name1 name2)]
   =>
-  (insert! (->Happy ?name1 :high)))
+  (insert! (->Happy ?name1)))
 
 (-> (mk-session 'genifer6.core)
     (insert (->Loves "John" "Mary")
             (->Loves "Mary" "John")
-            (->Happy "Mary" :high))
+            (->Happy "Mary"))
     (fire-rules))
 
-
 ;; General form of a "predicate" declaration:
-(defrecord P0001 [V0001 V0002 V0003])
+;; * The format is generic and does not require learning, can be fixed initially
+(defrecord P0001 [L1-P0001 L2-P0001 L3-P0001])
 
 ;; General form of a "rule" declaration:
-;(defrule R0001
-;  [P0001 (= ?
-;  =>
-;  [])
+(defrule R0001
+  [P0001 (= ?V0001 L1-P0001) (= ?V0002 L2-P0001)]
+  [P0001 (= ?V0002 L1-P0001) (= ?V0001 L2-P0001)]
+  =>
+  (insert! (->P0003 ?V0001)))
+;; The gene would be:
+;; P1 ?V1 ?V2 ^ P1 ?V2 ?V1 => P3 ?V1
+;; The format as list:
+;; [ [] [] ] => []
